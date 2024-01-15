cff1c87a5f55fb9c95a5b4c85fcb7260e0a9996c
= <=>
2024-01-16 01:13:08 +0300

    Basic socket

---
+++ baseclient.py
@@ -0,0 +1,11 @@
+import socket 
+
+HOST = "localhost"
+PORT = 8888
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+sock.send(b"Merhabalar")
+print(sock.recv(1024))
+sock.close()
---
+++ baseserver.py
@@ -0,0 +1,18 @@
+import socket 
+
+host = "localhost"
+port = 8888
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((host, port))
+    sock.listen()
+    count = 0
+
+    while True:   
+        conn, addr = sock.accept()
+        count = count + 1
+        conn.sendall(b"Hello Everyone ")
+        print(f"{count}: Message from {addr}")
+        
+    
+    
